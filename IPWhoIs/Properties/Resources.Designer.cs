4691374cdaa94b4dc9d26e9c2dd2138a4e729983
Zhan <Zhan.Kariahin@gmail.com>
2025-04-01 05:32:32 +0300

    +

--- IPWhoIs/Properties/Resources.Designer.cs
+++ IPWhoIs/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace IPWhoIs.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Screenshot_1 {
-            get {
-                object obj = ResourceManager.GetObject("Screenshot_1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
